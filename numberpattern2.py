bbffcf60012b01b421ab6ada27cde44dc2a942e1
Vikash Jha <jha2020vikash@yahoo.com>
2021-07-20 14:07:23 +0545

    2nd pattern added

---
+++ numberpattern2.py
@@ -0,0 +1,24 @@
+"""
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+11
+202
+3003
+"""
+
+n = int(input())
+
+row = 0
+while(row < n):
+    col = 0
+    while(col <= row):
+
+        if row == 0: 
+            print(1, end = ' ')
+        elif row == col or col == 0:
+            print(row, end = ' ')
+        else:
+            print(0, end = ' ')
+        col = col+1
+    row = row + 1
